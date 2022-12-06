960f58e2f4fbbacb57d41915e1cd3e571737e24f
Martijn Muijsers <martijnmuijsers@live.nl>
2022-12-06 11:15:20 +0100

    Day 4

---
+++ src/main/kotlin/Day4B.kt
@@ -0,0 +1,10 @@
+import java.io.File
+
+fun main() {
+	println(File("input/4.txt").useLines { it.count {
+		val ranges = it.split(",").map { it.split("-").map { it.toInt() } }
+		for (i in 0..1)
+			if (ranges[i][0] <= ranges[1 - i][1] && ranges[i][1] >= ranges[1 - i][0]) return@count true
+		false
+	} })
+}
